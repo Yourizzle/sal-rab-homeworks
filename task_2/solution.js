d61f334e970c039d59435611fad5449649e0850e
yourizzle <dunaevyury@gmail.com>
2023-09-16 09:01:08 +0300

    task_2.1

--- task_2/solution.js
+++ task_2/solution.js
@@ -6,24 +6,24 @@ function calcShipping(sum, min, shipping) {
     // Задание №2.1. Рассчитать доставку
 
     // создайте переменную shippingSum
-    let shippingSum
+    let shippingSum;
 
     // если productsSum равно 0,
-    if productsSum == 0 {
+    if (productsSum == 0) {
         shippingSum = 0
-    }
+    };
     // то shippingSum присвоить значение 0
 
     // если productsSum Больше или равна freeShippingMinSum,
     // то shippingSum присвоить значение 0
-    else productsSum > 0 and < freeShippingMinSum {
+    else (productsSum > 0 and < freeShippingMinSum) {
         shippingSum = shippingPrice
-    }
+    };
     // если productsSum больше 0 и меньше freeShippingMinSum,
     // то shippingSum присвоить значение shippingPrice
-    else productsSum > 0 and < freeShippingMinSum {
+    else (productsSum > 0 and < freeShippingMinSum) {
         shippingPrice = shippingPrice
-    }
+    };
     // Конец решения задания №2.1.
 
     return shippingSum;
